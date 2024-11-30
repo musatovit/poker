2b651e78539ac5d65d59744a8c7def36eba25335
Dmitrii Musatov <82502671+musatovit@users.noreply.github.com>
2024-11-30 09:01:10 +0300

    update

--- src/Components/Rebuys.tsx
+++ src/Components/Rebuys.tsx
@@ -26,9 +26,9 @@ function Rebuys({rebuy, setRebuy, sumRebuy,setSumRebuy, setAllChips, isBuyIn = t
                     </>
                 )
                 : (<>
-                    <button onClick={() => plusRebuy(7500)}>7500</button>
-                    <button onClick={() => plusRebuy(15000)}>15000</button>
-                    <button onClick={() => plusRebuy(30000)}>30000</button>
+                    <button onClick={() => plusRebuy(25000)}>25000</button>
+                    {/*<button onClick={() => plusRebuy(15000)}>15000</button>*/}
+                    {/*<button onClick={() => plusRebuy(30000)}>30000</button>*/}
                 </>)
             }
             <button onClick={() => deleteRebuy()}>del</button>
